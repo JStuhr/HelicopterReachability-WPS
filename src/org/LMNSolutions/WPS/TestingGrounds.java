bcc638504072fdba83bba6076f93741007dc8236
Joseph Stuhr <JpDeathBlade@gmail.com>
2012-07-07 14:00:35 -0400

    no message

--- src/org/LMNSolutions/WPS/TestingGrounds.java
+++ src/org/LMNSolutions/WPS/TestingGrounds.java
@@ -1,33 +1,30 @@
 package org.LMNSolutions.WPS;
 
 import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.coverage.grid.GridGeometry2D;
 import org.geoserver.wps.gs.GeoServerProcess;
 import org.geoserver.wps.jts.DescribeParameter;
 import org.geoserver.wps.jts.DescribeProcess;
 import org.geoserver.wps.jts.DescribeResult;
-import org.joda.time.Duration;
 
 import com.vividsolutions.jts.geom.Geometry;
 
 
-@DescribeProcess(title = "HelicopterReachability", description = "Given a raster, find the area a helicopter can fly in.")
-public class HelicopterReachability implements GeoServerProcess {
+@DescribeProcess(title = "TestingGrounds", description = "Tests and junk.")
+public class TestingGrounds implements GeoServerProcess {
 
     @DescribeResult(name = "result", description = "The collection of result polygons.")
-    public Geometry execute(
+    public GridGeometry2D execute(
             @DescribeParameter(name = "raster", description = "The raster to check against.") GridCoverage2D _coverage,
     		@DescribeParameter(name = "startPoint", description = "The point to start at.") Geometry _startPoint)
             throws Exception {
-    	return calculateArea(_coverage, _startPoint);
+    	return test(_coverage, _startPoint);
     }
     
-    public static Geometry calculateArea(GridCoverage2D _coverage, Geometry _start) 
+    public static GridGeometry2D test(GridCoverage2D _coverage, Geometry _start) 
 	{
-    	return null;
+    	GridGeometry2D test = _coverage.getGridGeometry();
+    	
+    	return test;
 	}
-    
-    private static Geometry calculateImpassableAreas(Duration time, double elevation_m)
-    {
-    	return null;
-    }
 }
